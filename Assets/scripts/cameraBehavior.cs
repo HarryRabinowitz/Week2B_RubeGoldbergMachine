aa162ce1b226bb279e995683270988ce0b55282f
Harry Rabinowitz <harry.rabinowitz@gmail.com>
2015-07-20 17:06:48 -0400

    Week2B Rube Goldberg Machine, Complete V1.0

---
+++ Assets/scripts/cameraBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraBehavior : MonoBehaviour 
+{
+
+	public GameObject cameraCube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		cameraCube.transform.position += new Vector3 (0.55f, 0f, 0f) * Time.deltaTime;
+		//cameraCube.transform.position += new Vector3 ((0.0f, -0.5f, 0f) * Time.time > 5f);
+	}
+}
---
+++ Assets/scripts/particleSystemBehavior.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleSystemBehavior : MonoBehaviour
+{
+
+
+	public GameObject particleEmission;
+	bool haveITriggered = false;
+	
+	//void OnTriggerEnter (Collider other)
+	//{
+	//	Debug.Log ("Object Entered the trigger");
+	//	enableEmission = true;
+
+
+
+	void OnTriggerEnter ()
+	{
+		haveITriggered = true;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		particleEmission.SetActive (false);
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (haveITriggered == true) {
+			particleEmission.SetActive (true);
+		}
+	
+	}
+
+}
+
+
